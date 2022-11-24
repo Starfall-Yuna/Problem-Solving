6acf3c73685dbb0e417a9f6df478ffc2563b3cc0
Yuna Lee(Smary) <80337742+Starfall-Yuna@users.noreply.github.com>
2022-11-24 17:51:28 +0900

    solved 2558 (Math)

---
+++ Baekjoon/Math/2558_A+B-2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+//백준 2558번 코드
+//풀이:: https://smary-it.tistory.com/35
+
+int main(void)
+{
+	int a, b;
+	scanf("%d\n%d", &a, &b);
+	printf("%d", a + b);
+}
