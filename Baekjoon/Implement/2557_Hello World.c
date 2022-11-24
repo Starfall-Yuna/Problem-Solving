059261619e96bad0c7acbb2da02cfd93d016dcbc
Yuna Lee(Smary) <80337742+Starfall-Yuna@users.noreply.github.com>
2022-11-24 17:50:04 +0900

    solved 2557 (Implement)

---
+++ Baekjoon/Implement/2557_Hello World.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+//백준 2557번 코드
+//풀이:: https://smary-it.tistory.com/34
+
+int main() {
+	printf("Hello World!\n");
+}
